915b90d55cd9205622a1007fac5f3c26c1099d80
JungYeol <zoo3309@naver.com>
2021-02-24 23:22:57 +0900

    Request Scope 예제

---
+++ src/main/java/hello/core/common/web/LogDemoService.java
@@ -0,0 +1,18 @@
+package hello.core.common.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+        System.out.println("myLogger = " + myLogger.getClass());
+    }
+}
